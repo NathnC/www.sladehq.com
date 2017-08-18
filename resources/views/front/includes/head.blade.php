98d5aabb529ac9740d3f7205a08d67bb115e5f68
nathanc <izeatfishz1@gmail.com>
2017-08-18 20:24:14 +0100

    fixed the sladehq 43543 thing, removed pace.js

--- resources/views/front/includes/head.blade.php
+++ resources/views/front/includes/head.blade.php
@@ -44,8 +44,5 @@
         <!-- Favicon -->
         <link href="images/branding/favicon.ico" rel="icon" type="image/png" sizes="16x16">
 
-        <script src="js/pace.js"></script>
-        <link href="css/pace.css" rel="stylesheet" />
-
 
 </head>
